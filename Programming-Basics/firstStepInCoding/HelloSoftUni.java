c919a1a61021ab7334eb35104ea5e8be0b4ebda7
Vasil Petkov <vasetoboarding@gmail.com>
2019-03-31 13:37:51 +0300

    Add homework from "First steps in coding" lesson.

---
+++ Programming-Basics/firstStepInCoding/HelloSoftUni.java
@@ -0,0 +1,10 @@
+package firstStepInCoding;
+
+public class HelloSoftUni {
+    public static void main(String[] args) {
+        System.out.println("Hello SoftUni");
+    }
+
+
+}
+
---
+++ Programming-Basics/firstStepInCoding/Number1To20.java
@@ -0,0 +1,10 @@
+package firstStepInCoding;
+
+public class Number1To20 {
+    public static void main(String[] args) {
+        for(int i = 1;  i <= 20; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
